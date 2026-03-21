af6e85e8f507c7a076a3e9c05932944c84eef3e9
codewithwambi <wambielvis59@gmail.com>
2026-03-21 15:42:50 +0000

    added workplace supervisor profile,email,

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -11,6 +11,8 @@ class User(AbstractUser):
     )
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
 
+    email =models.EmailField(unique=True)
+
     def __str__(self):
         return f"{self.username} ({self.get_role_display()})"
 
@@ -20,10 +22,22 @@ class StudentProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_profile')
     registration_number = models.CharField(max_length=50, unique=True)
     course = models.CharField(max_length=100)
-    year_of_study = models.IntegerField(default=1)
+    year_of_study = models.PositiveBigIntegerField()
+    is_eligible=models.BooleanField(default=False)
+    cv=models.FileField(upload_to='cvs/', null=True , blank=True)
 
     def __str__(self):
-        return self.user.username
+        return f"{self.user.get_full_name()} - {self.registration_number}"
+    
+class WorkplaceSupervisorProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='workplace_profile')
+    # This will link to the Organization model we create next
+    organizations= models.ForeignKey('organizations.Organization', on_delete=models.CASCADE, related_name='supervisors', null=True)
+    job_title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.user.get_full_name()} ({self.job_title})"
+
 
 class AcademicSupervisorProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='academic_profile')
--- backend/accounts/signals.py
+++ backend/accounts/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import User, StudentProfile, AcademicSupervisorProfile
+from .models import User, StudentProfile, AcademicSupervisorProfile,WorkplaceSupervisorProfile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -9,3 +9,14 @@ def create_user_profile(sender, instance, created, **kwargs):
             StudentProfile.objects.create(user=instance)
         elif instance.role == 'academic_sup':
             AcademicSupervisorProfile.objects.create(user=instance)
+        elif instance.role == 'workplace_sup':
+            WorkplaceSupervisorProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    if instance.role == 'student' and hasattr(instance, 'student_profile'):
+        instance.student_profile.save()
+    elif instance.role == 'academic_sup' and hasattr(instance, 'academic_profile'):
+        instance.academic_profile.save()
+    elif instance.role == 'workplace_sup' and hasattr(instance, 'workplace_profile'):
+        instance.workplace_profile.save()
