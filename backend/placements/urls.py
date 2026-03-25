dbfc1f54f19b60bad15967a6bcb8b567ac40b935
codewithwambi <wambielvis59@gmail.com>
2026-03-25 22:21:00 +0000

    modified placement views.py and urls

--- backend/placements/urls.py
+++ backend/placements/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PlacementListCreateView, WeeklyLogListCreateView,SupervisorLogReviewView
+from .views import PlacementListCreateView, WeeklyLogListCreateView,SupervisorLogReviewView,AssignedStudentsListView
 
 urlpatterns = [
     path('applications/', PlacementListCreateView.as_view(), name='placement-list'),
     path('logs/', WeeklyLogListCreateView.as_view(), name='weekly-logs'),
     path('logs/review/<int:pk>/', SupervisorLogReviewView.as_view(), name='log-review'),
+    path('assigned-students/', AssignedStudentsListView.as_view(), name='assigned-students'),
 ]
 
